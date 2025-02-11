f806e54da96e1a06421beac71ff3e195f6028e15
dario <dario@dario-IdeaPad-3-14ALC6>
2025-02-11 14:30:25 -0500

    Carga inicial de documentos al servidor Git.

---
+++ src/main/java/com/mycompany/set01/Set01.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.set01;
+
+/**
+ *
+ * @author dario
+ */
+public class Set01 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
